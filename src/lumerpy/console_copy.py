c00ef77abf9089f7fcc6128fcfdda9706d76341c
OscarxChen <42312874+OscarXChen@users.noreply.github.com>
2025-03-31 01:19:21 +0800

    进行大量数据生成工作

--- src/lumerpy/console_copy.py
+++ src/lumerpy/console_copy.py
@@ -15,7 +15,7 @@ lupy.tools.check_path_and_file(file_path=file_path, file_name=file_name)
 lupy.setup_paths(api_path, file_path, file_name)  # 设置路径到库
 
 # --------------------基本设置结束--------------------
-fdtd_instance = lupy.get_fdtd_instance(hide=True, solution_type="FDTD")  # 创建fdtd实例，这应该是第一个实例，hide=True时，隐藏窗口
+fdtd_instance = lupy.get_fdtd_instance(hide=False, solution_type="FDTD")  # 创建fdtd实例，这应该是第一个实例，hide=True时，隐藏窗口
 # lupy.version()  # 测试一下是否成功
 FD = lupy.get_existing_fdtd_instance()  # 返回创建的实例，以便使用lumapi
 if not FD:
@@ -27,7 +27,7 @@ import numpy as np
 
 lupy.plot_initialize()
 
-Edatas = FD.getresult("local_outputs", "E")
+# Edatas = FD.getresult("local_outputs", "E")
 z_fixed = 0.11e-6
 
 # out_y_span = out_y_pixel_scale * scale_ratio
@@ -36,15 +36,15 @@ z_fixed = 0.11e-6
 # 	out_y_start_ls.append(out_y_start_ls_temp[i] * scale_ratio + extra_gap_y)
 # 	print(f"输出位置[{i}]：{out_y_start_ls[i]},{out_y_start_ls[i] + out_y_span}")
 
-# 选择好输出范围即可
-selected_ranges = np.array([
-	[0e-6, 6e-6],
-	[12e-6, 18e-6]
-])
-
-idx, energy_list = lupy.get_simple_out(selected_range=selected_ranges, power_name="local_outputs", z_fixed=z_fixed)
-output_energy_ls = [round(float(x), 4) for x in energy_list]
-print(f"输出区域是：{idx}，并且各输出值为：{output_energy_ls}")
+# # 选择好输出范围即可
+# selected_ranges = np.array([
+# 	[0e-6, 6e-6],
+# 	[12e-6, 18e-6]
+# ])
+#
+# idx, energy_list = lupy.get_simple_out(selected_range=selected_ranges, power_name="local_outputs", z_fixed=z_fixed)
+# output_energy_ls = [round(float(x), 4) for x in energy_list]
+# print(f"输出区域是：{idx}，并且各输出值为：{output_energy_ls}")
 
 # E_list, coord_list, z_used, energy_list = lupy.select_E_component_by_range_from_dataset(
 # 	Edatas, axis_name='y', component='Ey', fixed_axis_name='z',
@@ -54,3 +54,9 @@ print(f"输出区域是：{idx}，并且各输出值为：{output_energy_ls}")
 # print("能量最大的区域是 Region", idx + 1)
 
 # input("输入回车结束")
+
+power_name="local_outputs"
+Edatas = FD.getresult(power_name, "E")
+E_list, coord_list, z_used, energy_list = lupy.select_E_component_by_range_from_dataset(
+	Edatas, axis_name="y", component="Ey",min_val=FD.getnamed("FDTD","y min"),max_val=FD.getnamed("FDTD","y max"),
+	plot=True, Energyshow=True, plot_energy=True)
--- src/lumerpy/data_process.py
+++ src/lumerpy/data_process.py
@@ -30,7 +30,7 @@ def select_E_component_by_range_from_dataset(
 		fixed_axis_value=None,
 		plot=False,
 		Energyshow=True,
-		selected_range=None, plot_energy=False
+		selected_range=None, plot_energy=False,save_path=None
 ):
 	axis_map = {'x': 0, 'y': 1, 'z': 2}
 	comp_map = {'Ex': 0, 'Ey': 1, 'Ez': 2}
@@ -171,21 +171,28 @@ def select_E_component_by_range_from_dataset(
 		plt.tight_layout()
 		if plot_energy:
 			plt.show()
-
+			if save_path:
+				import os
+				os.makedirs(save_path, exist_ok=True)
+				import time
+				current_time = time.strftime("%m%d-%H%M")
+				fig.savefig(f"{save_path}{current_time}_{component}.png", dpi=300)
+				print(f"✅ 所有能量图已保存至 {save_path}_{component}.png")
 		for i, e in enumerate(energy_all):
 			print(f"区域 {i} 累计 {component}² 能量为: {e:.4e}")
+
 	return E_all, coord_all, fixed_coord_value, energy_all if Energyshow else None
 
 def get_simple_out(selected_range, power_name="local_outputs", z_fixed=0.11e-6,
 				   plot=False, Energyshow=True, plot_energy=False,
-				   axis_name='y', component='Ey', fixed_axis_name='z'):
+				   axis_name='y', component='Ey', fixed_axis_name='z',save_path=False):
 	FD = get_fdtd_instance()
 	Edatas = FD.getresult(power_name, "E")
 
 	E_list, coord_list, z_used, energy_list = select_E_component_by_range_from_dataset(
 		Edatas, axis_name=axis_name, component=component, fixed_axis_name=fixed_axis_name,
 		fixed_axis_value=z_fixed, selected_range=selected_range,
-		plot=plot, Energyshow=Energyshow, plot_energy=plot_energy)
+		plot=plot, Energyshow=Energyshow, plot_energy=plot_energy,save_path=save_path)
 
 	# print(energy_list)
 	idx = int(np.argmax(energy_list))
@@ -205,3 +212,74 @@ def get_simple_out(selected_range, power_name="local_outputs", z_fixed=0.11e-6,
 # 	# x，y，z，f，(Ex,Ey,Ez)
 # 	# 我现在希望：
 # 	# 选取所有x在我指定的范围（例如：index=[3,5]）中的Ey数据，如何做？
+
+def get_results(size=(1, 50), channals_output=2, duty_cycle=0.5, margins_cycle=(0, 0, 0, 0), power_name="local_outputs",
+				period=0.5e-6, width=0.2e-6, z_fixed=0.11e-6,
+				file_path=r"E:\0_Work_Documents\Simulation\lumerpy\03_cat",
+				file_name=r"m00_temp.fsp",save_path=False):
+	import sys
+	import os
+
+	# 用户在这里设置 API 和文件路径
+	api_path = r"C:/Program Files/Lumerical/v241/api/python"
+	# file_path = r"E:\0_Work_Documents\Simulation\lumerpy\03_cat"
+	# file_name = r"m01_wg.fsp"
+	# file_name = r"m00_temp.fsp"
+	sys.path.append(os.path.normpath(api_path))  # 添加 API 路径以确保可以成功导入 lumapi
+	import lumerpy as lupy
+
+	lupy.tools.check_path_and_file(file_path=file_path, file_name=file_name)
+	# import lumapi		# lupy库中已经包含了lumapi的导入，不需要额外导入lumapi
+	lupy.setup_paths(api_path, file_path, file_name)  # 设置路径到库
+
+	# --------------------基本设置结束--------------------
+	fdtd_instance = lupy.get_fdtd_instance(hide=True, solution_type="FDTD")  # 创建fdtd实例，这应该是第一个实例，hide=True时，隐藏窗口
+	# lupy.version()  # 测试一下是否成功
+	FD = lupy.get_existing_fdtd_instance()  # 返回创建的实例，以便使用lumapi
+	if not FD:
+		print("未正确创建实例，请检查")
+	u = 1e-6
+
+	# --------------------现在既可以调用lumapi，也可以调用lupy库--------------------
+	import numpy as np
+
+	lupy.plot_initialize()
+	Edatas = FD.getresult(power_name, "E")
+	out_y_ls_temp, out_y_start_ls_temp, out_y_pixel_scale = lupy.tools.get_single_inputs_center_x(
+		channels=channals_output,
+		data_single_scale=size,
+		duty_cycle=duty_cycle,
+		margins_cycle=margins_cycle)
+
+	fdtd_y_span = FD.getnamed("FDTD", "y span")
+	scale_ratio = (fdtd_y_span / size[1])
+	extra_gap_y = (period - width) / 2  # 额外抬高半个槽和槽之间的间距
+	out_y_ls = []
+	starts_ls = []
+	out_y_start_ls = []
+	out_y_range = np.zeros((len(out_y_ls_temp), 2))
+	out_y_span = out_y_pixel_scale * scale_ratio
+	for i in range(len(out_y_ls_temp)):
+		out_y_ls.append(out_y_ls_temp[i] * scale_ratio + extra_gap_y)
+		out_y_start_ls.append(out_y_start_ls_temp[i] * scale_ratio + extra_gap_y)
+		out_y_range[i, :] = out_y_start_ls[i], out_y_start_ls[i] + out_y_span
+	# print(f"输出位置[{i}]：{out_y_start_ls[i]},{out_y_start_ls[i] + out_y_span}")
+	# print(out_y_range)
+	# 选择好输出范围即可
+	# selected_ranges = np.array([
+	# 	[0e-6, 6e-6],
+	# 	[12e-6, 18e-6]
+	# ])
+
+	idx, energy_list = lupy.get_simple_out(selected_range=out_y_range, power_name=power_name, z_fixed=z_fixed,
+										   plot=True, plot_energy=True,save_path=save_path)
+	output_energy_ls = [round(float(x), 4) for x in energy_list]
+	# print(f"输出区域是：{idx}，并且各输出值为：{output_energy_ls}")
+
+
+	for i in range(out_y_range.shape[0]):
+		area_start,area_end=out_y_range[i,:]
+		print(f"区域 {i} 范围：{area_start*1e6:.2f},\t{area_end*1e6:.2f}")
+	# print(f"可能输出区域为：{out_y_range}")
+	print(f"输出区域是：区域 {idx}，并且各区域输出值为：{output_energy_ls}")
+	return idx,output_energy_ls
